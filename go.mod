73f234fed8853e80d84b52e0d345f1798b7ca3a9
sam-antino <sam.s@antino.io>
2023-10-02 22:49:31 +0530

    tests for adspace service

--- go.mod
+++ go.mod
@@ -8,7 +8,12 @@ require (
 	gorm.io/gorm v1.25.4
 )
 
-require github.com/go-sql-driver/mysql v1.7.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+)
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
@@ -16,6 +21,7 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1
+	github.com/go-playground/assert/v2 v2.2.0
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
---
+++ services/adspace/adspace_impl_test.go
@@ -0,0 +1,136 @@
+package adspace
+
+import (
+	"PERSONAL/ad_space_auction_service/configs"
+	"PERSONAL/ad_space_auction_service/constants"
+	"PERSONAL/ad_space_auction_service/database"
+	"PERSONAL/ad_space_auction_service/models"
+	"PERSONAL/ad_space_auction_service/models/entities"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	configs.LoadConfig()
+	database.Connect()
+	database.SyncDatabase()
+}
+
+func TestCreateAdspace(t *testing.T) {
+	req := models.AdspaceReq{
+		Name:           "sam sunil",
+		Description:    "test decription",
+		BasePrice:      10.05,
+		AuctionEndTime: "02-10-2023 19:05:00",
+		ExpiredAt:      "02-10-2023 19:10:00",
+	}
+	res, err := AdspaceImplementations{}.CreateAdspace(req)
+	assert.Nil(t, err)
+	assert.Equal(t, req.Name, res.Name)
+	assert.Equal(t, req.Description, res.Description)
+	assert.Equal(t, req.BasePrice, res.BasePrice)
+	assert.Equal(t, req.AuctionEndTime, res.AuctionEndTime.Format(constants.Time_Format_DD_MM_YYYY_WITH_COLON_HH_MM_SS))
+	assert.Equal(t, req.ExpiredAt, res.ExpiredAt.Format(constants.Time_Format_DD_MM_YYYY_WITH_COLON_HH_MM_SS))
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", res.Uuid).Delete(&entities.AdSpaces{})
+	})
+}
+
+func TestGetAllAvailableAdspace(t *testing.T) {
+	// creating an adspace before listing
+	req := models.AdspaceReq{
+		Name:           "sam sunil",
+		Description:    "test decription",
+		BasePrice:      10.05,
+		AuctionEndTime: "02-10-2023 19:05:00",
+		ExpiredAt:      "02-10-2023 19:10:00",
+	}
+	createRes, err := AdspaceImplementations{}.CreateAdspace(req)
+	assert.Nil(t, err)
+
+	resp, err := AdspaceImplementations{}.GetAllAvailableAdspace()
+	assert.Nil(t, err)
+	assert.GreaterOrEqual(t, resp.Count, 1)
+	found := false
+	for _, adspace := range resp.Data {
+		if adspace.Uuid == createRes.Uuid {
+			found = true
+			break
+		}
+	}
+	assert.Equal(t, found, true)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createRes.Uuid).Delete(&entities.AdSpaces{})
+	})
+}
+
+func TestGetAdspaceById(t *testing.T) {
+	// Creating an adspace before fetching by its uuid
+	req := models.AdspaceReq{
+		Name:           "sam sunil",
+		Description:    "test decription",
+		BasePrice:      10.05,
+		AuctionEndTime: "02-10-2023 19:05:00",
+		ExpiredAt:      "02-10-2023 19:10:00",
+	}
+	createRes, err := AdspaceImplementations{}.CreateAdspace(req)
+	assert.Nil(t, err)
+
+	resp, err := AdspaceImplementations{}.GetAdspaceById(createRes.Uuid)
+	assert.Nil(t, err)
+	assert.Equal(t, resp.Name, createRes.Name)
+	assert.Equal(t, resp.Description, createRes.Description)
+	assert.Equal(t, resp.BasePrice, createRes.BasePrice)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createRes.Uuid).Delete(&entities.AdSpaces{})
+	})
+
+}
+
+func TestUpdateAdspaceById(t *testing.T) {
+	// Creating an adspace before updating by its uuid
+	req := models.AdspaceReq{
+		Name:           "sam sunil",
+		Description:    "test decription",
+		BasePrice:      10.05,
+		AuctionEndTime: "02-10-2023 19:05:00",
+		ExpiredAt:      "02-10-2023 19:10:00",
+	}
+	createRes, err := AdspaceImplementations{}.CreateAdspace(req)
+	assert.Nil(t, err)
+
+	resp, err := AdspaceImplementations{}.UpdateAdspaceById(createRes.Uuid, models.AdspaceReq{
+		Name:        "updated name",
+		Description: "updated description",
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, "updated name", resp.Name)
+	assert.Equal(t, "updated description", resp.Description)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createRes.Uuid).Delete(&entities.AdSpaces{})
+	})
+}
+
+func TestDeleteAdspaceById(t *testing.T) {
+	req := models.AdspaceReq{
+		Name:           "sam sunil",
+		Description:    "test decription",
+		BasePrice:      10.05,
+		AuctionEndTime: "02-10-2023 19:05:00",
+		ExpiredAt:      "02-10-2023 19:10:00",
+	}
+	createRes, err := AdspaceImplementations{}.CreateAdspace(req)
+	assert.Nil(t, err)
+
+	resp, err := AdspaceImplementations{}.DeleteAdspaceById(createRes.Uuid)
+	assert.Nil(t, err)
+	assert.Contains(t, resp.Message, createRes.Uuid)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createRes.Uuid).Delete(&entities.AdSpaces{})
+	})
+}
