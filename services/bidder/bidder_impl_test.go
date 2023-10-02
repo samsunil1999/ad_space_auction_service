b68c0373019023404352ba7a9c6a3f7d71d8f324
sam-antino <sam.s@antino.io>
2023-10-02 23:30:28 +0530

    tests for bidder service added

---
+++ services/bidder/bidder_impl_test.go
@@ -0,0 +1,122 @@
+package bidder
+
+import (
+	"PERSONAL/ad_space_auction_service/configs"
+	"PERSONAL/ad_space_auction_service/database"
+	"PERSONAL/ad_space_auction_service/models"
+	"PERSONAL/ad_space_auction_service/models/entities"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	configs.LoadConfig()
+	database.Connect()
+	database.SyncDatabase()
+}
+
+func TestRegisterBidder(t *testing.T) {
+	req := models.BidderReq{
+		Name:        "sam sunil",
+		Email:       "xyz@gmail.com",
+		PhoneNumber: "1234567890",
+	}
+	resp, err := BidderImplementations{}.RegisterBidder(req)
+	assert.Nil(t, err)
+	assert.Equal(t, req.Name, resp.Name)
+	assert.Equal(t, req.Email, resp.Email)
+	assert.Equal(t, req.PhoneNumber, resp.PhoneNumber)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", resp.Uuid).Delete(&entities.Bidders{})
+	})
+}
+
+func TestGetAllBidders(t *testing.T) {
+	createReq := models.BidderReq{
+		Name:        "sam sunil",
+		Email:       "xyz@gmail.com",
+		PhoneNumber: "1234567890",
+	}
+	createResp, err := BidderImplementations{}.RegisterBidder(createReq)
+	assert.Nil(t, err)
+
+	resp, err := BidderImplementations{}.GetAllBidders()
+	assert.Nil(t, err)
+	assert.GreaterOrEqual(t, resp.Count, 1)
+	found := false
+	for _, bdr := range resp.Data {
+		if bdr.Uuid == createResp.Uuid {
+			found = true
+			break
+		}
+	}
+	assert.True(t, found)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createResp.Uuid).Delete(&entities.Bidders{})
+	})
+}
+
+func TestGetBidderById(t *testing.T) {
+	createReq := models.BidderReq{
+		Name:        "sam sunil",
+		Email:       "xyz@gmail.com",
+		PhoneNumber: "1234567890",
+	}
+	createResp, err := BidderImplementations{}.RegisterBidder(createReq)
+	assert.Nil(t, err)
+
+	resp, err := BidderImplementations{}.GetBidderById(createResp.Uuid)
+	assert.Nil(t, err)
+	assert.Equal(t, createReq.Name, resp.Name)
+	assert.Equal(t, createReq.Email, resp.Email)
+	assert.Equal(t, createReq.PhoneNumber, resp.PhoneNumber)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createResp.Uuid).Delete(&entities.Bidders{})
+	})
+}
+
+func TestUpdateBidderDetails(t *testing.T) {
+	createReq := models.BidderReq{
+		Name:        "sam sunil",
+		Email:       "xyz@gmail.com",
+		PhoneNumber: "1234567890",
+	}
+	createResp, err := BidderImplementations{}.RegisterBidder(createReq)
+	assert.Nil(t, err)
+
+	req := models.BidderReq{
+		Name:  "new name sam",
+		Email: "new@gmail.com",
+	}
+	resp, err := BidderImplementations{}.UpdateBidderDetails(createResp.Uuid, req)
+	assert.Nil(t, err)
+	assert.Equal(t, req.Name, resp.Name)
+	assert.Equal(t, req.Email, resp.Email)
+	assert.Equal(t, createResp.PhoneNumber, resp.PhoneNumber)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createResp.Uuid).Delete(&entities.Bidders{})
+	})
+}
+
+func TestDeleteBidder(t *testing.T) {
+	createReq := models.BidderReq{
+		Name:        "sam sunil",
+		Email:       "xyz@gmail.com",
+		PhoneNumber: "1234567890",
+	}
+	createResp, err := BidderImplementations{}.RegisterBidder(createReq)
+	assert.Nil(t, err)
+
+	resp, err := BidderImplementations{}.DeleteBidder(createResp.Uuid)
+	assert.Nil(t, err)
+	assert.Contains(t, resp, createResp.Uuid)
+
+	t.Cleanup(func() {
+		database.Db.Where("uuid = ?", createResp.Uuid).Delete(&entities.Bidders{})
+	})
+}
